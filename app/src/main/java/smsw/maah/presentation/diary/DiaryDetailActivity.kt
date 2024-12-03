ce93376103cf3b967d81159830291be22952e980
yeonee <leenayeon@sookmyung.ac.kr>
2024-12-03 11:03:18 +0900

    [Feat/#15] 일기 본문 배경 추가

---
+++ app/src/main/java/smsw/maah/presentation/diary/DiaryDetailActivity.kt
@@ -0,0 +1,12 @@
+package smsw.maah.presentation.diary
+
+import android.os.Bundle
+import smsw.maah.databinding.ActivityDiaryBinding
+import smsw.maah.util.base.BindingActivity
+
+class DiaryDetailActivity :
+    BindingActivity<ActivityDiaryBinding>({ ActivityDiaryBinding.inflate(it) }) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
