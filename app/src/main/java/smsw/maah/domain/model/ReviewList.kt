2c8ed9c4c2570c134e680b2eb6f123afbc5c9b69
ljinny <104649081+ljinny@users.noreply.github.com>
2024-12-19 21:56:54 +0900

    [Feat/#44] 리뷰 id 추가

--- app/src/main/java/smsw/maah/domain/model/ReviewList.kt
+++ app/src/main/java/smsw/maah/domain/model/ReviewList.kt
@@ -2,5 +2,6 @@ package smsw.maah.domain.model
 
 data class ReviewList(
     val hospitalName: String, // 병원 이름
-    val comment: List<String> // 방문자 리뷰
+    val comment: List<String>, // 방문자 리뷰
+    val reviewId: String
 )
